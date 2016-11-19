d7fddc6b1023236aaafaa2292f80faee243baf8f
Shaun Grech <shaun.grech13@gmail.com>
2016-11-19 13:55:10 +0100

    index on master: 9e6c7db Updated Readme

---
+++ XamarinFormsPresentations/Pages/Map/MapPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsPresentations
+{
+	public class MapPage : ContentPage
+	{
+		public MapPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
